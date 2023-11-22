3fd9e3fdcc95171bc8732876cbeec608e151f68e
André Rosatto <linus1178@gmail.com>
2023-11-22 16:22:24 -0300

    Ajustes

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
-import '../css/User.css';
-
 export default function User({ user, setSelectedUser }) {
 	return (
-		<li className='User'>
+		<li>
 			<img src={'../assets/profile_pics/' + user.pic} alt={'Imagem de perfil de ' + user.name} />
-			<div className='User__info'>
+			<div className='info'>
 				<p>{user.name}</p>
-				<div className='User__id-wrapper'>
-					<p className='User__id'>ID: {user.id}</p>
+				<div className='id-wrapper'>
+					<p className='id'>ID: {user.id}</p>
 					<p>Username: {user.username}</p>
 				</div>
 			</div>
